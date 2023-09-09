f54330299161a3058b6252e0197a0be7ac54830a
jade <jade.dasilva.lima.jds@gmail.com>
2023-09-09 08:38:34 -0400

    mvp scrumSpeaker

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,13 @@
-console.log("This is a popup!");
+const teamMembers = ["firstName 1", "firstName 2", "firstName 3", "firstName 4", "firstName 5"];
+
+document.addEventListener("DOMContentLoaded", function () {
+  const selectButton = document.getElementById("ss-select-button");
+  const selectedMemberParagraph = document.getElementById("selectedMember");
+
+  selectButton.addEventListener("click", function () {
+    const randomIndex = Math.floor(Math.random() * teamMembers.length);
+    const selectedMember = teamMembers[randomIndex];
+
+    selectedMemberParagraph.textContent = "Selected Team Member: " + selectedMember;
+  });
+});
