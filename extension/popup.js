db20fbb7bbc2a600ea8747217752f3052433d4d5
jade <jade.dasilva.lima.jds@gmail.com>
2023-09-16 13:12:38 -0400

    finished ui

--- extension/popup.js
+++ extension/popup.js
@@ -1,13 +1,33 @@
 const teamMembers = ["Team Member 1", "Team Member 2", "Team Member 3", "Team Member 4", "Team Member 5"];
+let count = 0;
+const selectButton = document.getElementById("ss-select-button");
+const selectedMemberParagraph = document.getElementById("selectedMember");
+const exclamationPointText = document.getElementById("exclamationPoint");
 
 document.addEventListener("DOMContentLoaded", function () {
-  const selectButton = document.getElementById("ss-select-button");
-  const selectedMemberParagraph = document.getElementById("selectedMember");
-
   selectButton.addEventListener("click", function () {
-    const randomIndex = Math.floor(Math.random() * teamMembers.length);
-    const selectedMember = teamMembers[randomIndex];
+    resetTeamMemberSelection();
 
-    selectedMemberParagraph.textContent = selectedMember + " !";
+    let interval = setInterval(function() {  
+      selectRandomTeamMember();
+      count += 1;
+
+      if(count >= 7) {
+        clearInterval(interval);
+        exclamationPointText.textContent = " !";
+      }
+    }, 400);
   });
 });
+
+function selectRandomTeamMember() {
+  let randomIndex = Math.floor(Math.random() * teamMembers.length);
+  const selectedMember = teamMembers[randomIndex];
+  selectedMemberParagraph.textContent = selectedMember;
+}
+
+function resetTeamMemberSelection() {
+  selectedMemberParagraph.textContent = "";
+  exclamationPointText.textContent = "";
+  count = 0;
+}
